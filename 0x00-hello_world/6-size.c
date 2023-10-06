9b429de8bafa24070b8657551b3f9812bb4fbf96
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-06 13:15:10 -0700

    Message: Fixed output of 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c byte(s)", sizeof(char));
-	printf("Size of an int: %d byte(s)", sizeof(int));
-	printf("Size of a long int: %ld byte(s)",sizeof(long int));
-	printf("Size of a long long int: %lld byte(s)", sizeof(long long int));
-	printf("Size of a float: %f byte(s)",sizeof(float));
+	printf("Size of a char: %zu byte(s)", sizeof(char));
+	printf("Size of an int: %zu byte(s)", sizeof(int));
+	printf("Size of a long int: %zu byte(s)",sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)",sizeof(float));
 	return (0);
 }
