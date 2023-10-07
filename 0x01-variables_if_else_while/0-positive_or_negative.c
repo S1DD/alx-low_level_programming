2415213b752ec8948e7419a99c8b63f3e6e1278d
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-07 14:12:16 -0700

    Fixed betty style coding

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- *
  * main - Entry point
+ *
+ * Description: This program generates a random number and
+ * categorizes it as positive, negative, or zero.
  *
  * Return: Always 0 (Success)
  */
- int main(void)
- {
+int main(void)
+{
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if(n == 0)
+	if (n > 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is positive\n", n);
 	}
-	else if(n < 0)
+	else if (n == 0)
 	{
-		printf("%d is negative\n",n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is positive\n",n);
+		printf("%d is negative\n", n);
 	}
-	return (0);
- }
 
+	return (0);
+}
