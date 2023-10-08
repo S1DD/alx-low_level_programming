6253fc515c02dc9cc082e6f0eb853d742565e303
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-08 08:58:55 -0700

    Added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all lowercase letters except for 'q' and 'e'
+ *
+ * Description: This program prints all lowercase letters from 'a' to 'z'
+ *              except for 'q' and 'e'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; x <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
