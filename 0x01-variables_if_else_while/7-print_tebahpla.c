11de196b6e9b4cd797e8f10323a289d0b8e69502
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-08 12:43:38 -0700

    Added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet in reverse
+ *
+ * Description: This program prints the lowercase alphabet in reverse order,
+ * starting from 'z' and ending at 'a'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
