e4e16c5e01c7cfd2e4d248919fd2075400fb7d90
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-08 12:13:24 -0700

    Added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Description: This program prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
