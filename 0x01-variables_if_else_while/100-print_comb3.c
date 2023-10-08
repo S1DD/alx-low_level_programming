99ef37d14604e4a4a426d4074ccaf894af0c307a
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-08 13:23:37 -0700

    Added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digits
+ *
+ * Description: This program prints all possible different combinations of
+ * two digits from '01' to '89', separated by commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, n;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (n = '1'; n <= '9'; n++)
+		{
+			if (i < n)
+			{
+				putchar(i);
+				putchar(n);
+
+
+				if (i != '8' || (i == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
