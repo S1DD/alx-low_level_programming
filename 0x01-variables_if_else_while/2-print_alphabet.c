85e790e403b0d9fd35757fa75fce6c5d6014a342
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-08 02:46:08 -0700

    Fixed betty coding style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	    putchar(c);
-
+		putchar(c);
 	putchar('\n');
 	return (0);
 }
