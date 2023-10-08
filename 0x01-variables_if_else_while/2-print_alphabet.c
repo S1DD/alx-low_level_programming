90ff3eb8c9969afa7e9e59e026d857b6b64ac6d8
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-08 01:54:00 -0700

    Fixed betty coding style

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all lowercase letters from 'a' to 'z'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	    putchar(c);
+
+	putchar('\n');
+	return (0);
+}
