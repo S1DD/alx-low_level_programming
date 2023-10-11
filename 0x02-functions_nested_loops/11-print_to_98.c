ba98e02e02136dee9945921dc603aa7cef407366
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-11 16:42:59 -0700

    Updated 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,19 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d,", n);
 		}
 	}
 	else
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
+
 			printf("%d, ", n);
 		}
 	}
 
-	printf("\n");
+	printf("98\n");
 }
