0c93b52dafb4d1e12c8a8e671a22a66ba2b75ff1
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-11 02:41:54 -0700

    Added header guard to prevent double inclusion

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -8,16 +11,17 @@ int _islower(int c);
 
 int _isalpha(int c);
 
-int print_sign(int n)l
+int print_sign(int n);
 
 int _abs(int);
 
-int print_last_digit(int)
+int print_last_digit(int);
 
 void jack_bauer(void);
 
 void times_tables(void);
 
-int add(int,int);
+int add(int, int);
 
 void print_to_98(int n);
+#endif
