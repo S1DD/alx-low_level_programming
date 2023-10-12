c6c09ab5d6fdec600854f7fd92026a447f1d653b
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-11 18:10:31 -0700

    Updated 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,26 +7,37 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int x, y, z, u, d;
 
-	for (i = 0; i <= 9; i++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (y = 0; y <= 9; y++)
 		{
-			int result = i * j;
+			z = x * y;
 
-			_putchar(result + '0');
-			if (j < 9)
+			if (z > 9)
 			{
-				_putchar(',');
-				_putchar(' ');
-				if (result < 10)
-				{
-					_putchar(' ');
-				}
-			}
+				u = z % 10;
+				d = (z - u) / 10;
 
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
 		}
+
 		_putchar('\n');
 	}
 
