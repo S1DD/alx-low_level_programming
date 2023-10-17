26676ba56b66f6fa20db75e7bc6f06a697d25411
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-16 17:18:36 -0700

    Added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be modified
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+	char *b, line;
+
+	b = s;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+
+	for (k = 1; k < j; k++)
+	{
+		b++;
+	}
+
+	for (i = 0; i < (j / 2); i++)
+	{
+		line = s[i];
+		s[i] = *b;
+		*b = line;
+		b--;
+	}
+}
