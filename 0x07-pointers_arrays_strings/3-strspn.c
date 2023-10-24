e41a382426f3a61ba5f07c1a3ec8167349b1ccbb
Thabiso-M <thabisomolefe515@gmail.com>
2023-10-24 12:24:48 -0700

    Fixed betty coding style and documentation

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,19 +5,19 @@
  * @s: source string
  * @accept: accepted
  *
- * Return Number of bytes in the inital segment
+ * Return: Number of bytes in the inital segment
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0, j, k = 0;
 
-	while(accept[i])
+	while (accept[i])
 	{
 		j = 0;
 
-		while(s[j] != 32)
+		while (s[j] != 32)
 		{
-			if(accept[i] == s[j])
+			if (accept[i] == s[j])
 			{
 				k++;
 			}
