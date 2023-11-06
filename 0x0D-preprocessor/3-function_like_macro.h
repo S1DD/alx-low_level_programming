18b8a8a136a9900554a34fb7452f1ac377c11b3a
Thabiso-M <thabisomolefe515@gmail.com>
2023-11-06 13:59:49 -0800

    Contains a function-like macro ABS(x) that computes the absolute value of a number x.

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro.h
+ * Auth: Thabiso Molefe
+ * Desc: Function-like macro ABS(x) that computes the absolute value
+ * @x: The absolute value
+ * of a number x
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
