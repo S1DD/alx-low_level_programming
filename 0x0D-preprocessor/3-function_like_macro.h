37a840aa7630b3e15322cbfb84dfb501a333cd29
Thabiso-M <thabisomolefe515@gmail.com>
2023-11-06 14:39:28 -0800

    Fixed issue with calculation

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,10 +5,10 @@
  * File: 3-function_like_macro.h
  * Auth: Thabiso Molefe
  * Desc: Function-like macro ABS(x) that computes the absolute value
- * @x: The absolute value
  * of a number x
+ * @x: The absolute value
  */
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
 
 #endif
