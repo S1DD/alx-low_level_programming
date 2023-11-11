d1c16253aaad0bff964f94ae13949144bbb1027c
Thabiso-M <thabisomolefe515@gmail.com>
2023-11-10 16:27:25 -0800

    Fixed issue detected by betty regarding space required after a semicolon in a for loop

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *seperator, const unsigned int n, ...)
 
 	va_start(strings, n);
 
-	for (i = 0; i < n;i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char *);
 
