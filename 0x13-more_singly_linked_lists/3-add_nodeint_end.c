307b3cca0715cd13f6d526e4cbbe2e375ab78f72
Thabiso-M <thabisomolefe515@gmail.com>
2023-11-21 15:52:55 -0800

    Fixed issue that was causing the program to print in reverse

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,16 +10,25 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newNode;
+	listint_t *newNode, *last;
 
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
 
 	newNode->n = n;
-	newNode->next = *head;
+	newNode->next = NULL;
+	
+	if (*head == NULL)
+		*head = newNode;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = newNode;
 
-	*head = newNode;
+	}
 
-	return (newNode);
+	return (*head);
 }
