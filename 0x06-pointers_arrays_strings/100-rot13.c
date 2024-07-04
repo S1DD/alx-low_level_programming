754ec7afd0b15e7e772bcbd14e665d7e4ed9a6a0
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-07-04 16:31:35 -0700

    Updated

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,20 +8,39 @@
  */
 char *rot13(char *s)
 {
-	int i, j;
-	char data[] = "ABCDEFGHIJLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datar[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int idx1 = 0, idx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[idx1])
 	{
-		for (j = 0; j < 52; j++)
+		for (idx2 = 0; idx2 < 52; idx2++)
 		{
-			if (s[i] == data[j])
+			if (s[idx1] == alphabet[idx2])
 			{
-				s[i] = datar[j];
+				s[idx1] = rot13key[idx2];
 				break;
 			}
 		}
+
+		idx1++;
 	}
+
 	return (s);
 }
