1d624340608387d48d13983d8ee56fbcf2f674f2
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-07-05 08:42:13 -0700

    Fixed syntax errors

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	int i, tota; = 0;
+	int i, total = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
-			tota; += i;
+			total += i;
 	}
 
 	printf("%d\n", total);
