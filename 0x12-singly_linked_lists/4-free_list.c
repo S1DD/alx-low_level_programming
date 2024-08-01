65117f85d1c529f97fc0dfe7534873a4f1f47f6b
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-08-01 13:36:21 -0700

    Fixed 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,11 +10,12 @@ void free_list(list_t *head)
 {
 	list_t *current_node;
 
-	while ((current_node = head) != NULL)
+	while (current_node)
 	{
 		head = head->next;
 		free(current_node->str);
 		free(current_node);
+		head = current_node;
 	}
 
 }
