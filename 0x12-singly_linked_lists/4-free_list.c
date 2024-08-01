e1cbc9f6a6a8e50bc023a4d7b2e7b404afdf87da
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-08-01 13:57:02 -0700

    Corrected the freeing up of memory for the nodes

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,8 +11,9 @@ void free_list(list_t *head)
 {
 	list_t *current_node;
 
-	while ((current_node = head) != NULL)
+	while (head != NULL)
 	{
+		current = head;
 		head = head->next;
 		free(current_node->str);
 		free(current_node);
