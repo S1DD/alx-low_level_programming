6bf94f5e06ea8fc5b2f95af3ea4ea32e6ccec8d4
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-08-13 12:09:39 -0700

    Fixed errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+/**_strlen - counts the characters in a string
+ *
+ * @str: The string to be processed
+ * Return: length of a string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
 /**
  * append_text_to_file - Appends text at the end of a file.
  * @filename: A pointer to the name of the file.
@@ -11,26 +24,28 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int counter;
-	int rwr;
+	int fd, rwr;
 
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return(-1);
 	if (text_content != NULL)
 	{
-		for (counter = 0; text_content[counter];)
-			counter++;
+		rwr = write(fd, text_content, _strlen(text_content));
+		if (rwr == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+		return (1);
 	}
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-	rwr = write(fd, text_content, counter);
-
-	if (fd == -1 || rwr == -1)
-		return (-1);
-
-	close(fd);
-
-	return (1);
+	else
+	{
+		close(fd);
+		return (1);
+	}
 }
