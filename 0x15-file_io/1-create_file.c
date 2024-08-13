af48f22ed9412fb2643883426feb52bab4948551
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-08-13 13:05:49 -0700

    Removed typo that prevented compilation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,11 +24,11 @@ int _strlen(char *str)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int w_fd, int rwr;
+	int w_fd, rwr;
 
 	if (filename == NULL)
 		return (-1);
-	w_fd = open(filename, O_WRONLY | O_CREAT | O_TRUC, 0600);
+	w_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (w_fd == -1)
 		return (-1);
 	if (text_content != NULL)
