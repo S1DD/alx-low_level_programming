a828229992ece30a553739e68518e51384533ea8
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-10-15 07:17:09 -0700

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ *
+ * hash_table_create - Creates a new hash table
+ * @size: Size of the hash table
+ *
+ * Returns: A pointer to a new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t*) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}
+	return (ht);
+}
