b2ed33622c45911a9cde692ca7d608fa3471f68c
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-10-15 07:56:28 -0700

    Fixed indentation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,13 +1,14 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Returns the index at which the key/value pair should be stored in the array of the hash table
+ * key_index - Returns the index at which the key/value pair should
+ *		be stored in the array of the hash table
  * @key: The key used to search for the index in the hash table
  * @size: Size of the hash table
- * 
+ *
  * Return: The index of the key/value pair stored in the table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-  return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
